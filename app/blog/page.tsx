b3e99d91ed7e89920f20b9b3d2fca37941ca52e8
Amol Bhavsar <amol.bhavsar@splus.local>
2025-09-24 17:08:05 +0530

    Added data on Dashboard.

---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+export default async function Blogs() {
+    return <>
+    <div id="blogs">
+        <div className="blogPost">
+            <h3>Blog 1</h3>
+            <p>Blog Text</p>
+        </div>
+    </div>
+    </>
+}
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -20,6 +20,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Blogs', href: '/blog', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
